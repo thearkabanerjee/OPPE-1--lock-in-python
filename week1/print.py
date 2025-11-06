4d4f81630de1be10f0c714b25f97a04bd37ded7f
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-06 20:55:03 +0530

    week 1

--- week1/print.py
+++ week1/print.py
@@ -1,3 +1,14 @@
 # print statement
 
 # print statement helps you to write more print 
+
+# print 1 statements
+
+print ("hello world")
+
+
+# printing multiple message sbut in the same line 
+
+print ("hello world", "why me", "isagi")
+
+
