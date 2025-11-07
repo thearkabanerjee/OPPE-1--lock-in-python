79985a8051e261d8e1e213518ceb7ae2142b3f5d
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-07 21:50:26 +0530

    week 1

--- week1/strings.py
+++ week1/strings.py
@@ -14,3 +14,15 @@ print ("the value of the concatinated string is ", c)
 
 print (f'the value of c is {c}')
 
+
+
+# string slicing
+
+random_string = 'randomstring'
+
+element_of_random_string= random_string[1] # displays a
+
+print (element_of_random_string)
+
+slicing_of_the_string = random_string [1:3]
+print (slicing_of_the_string)
