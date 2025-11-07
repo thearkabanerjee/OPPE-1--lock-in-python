454ecdaca18d4594ac88629fba50d42fe94f2ff9
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-07 19:34:32 +0530

    week 1

--- week1/operators-expressions.py
+++ week1/operators-expressions.py
@@ -21,6 +21,14 @@ print (union_of_l1_l2) # that is the set version
 
 print (list (union_of_l1_l2)) # that is the list of the set version []
 
+# intersection of lists
+
+intersection_of_lists = set (l1) & set (l2)
+
+print (intersection_of_lists) # set version
+
+print (list(intersection_of_lists)) # list version of the thing
+
 
 # union - the common ones (symmetric difference operator)
 
