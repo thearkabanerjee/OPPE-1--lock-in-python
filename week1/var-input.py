edf5d6c80ebb5204e14a4e2750b049596c92b895
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-07 10:22:32 +0530

    week 1

--- week1/var-input.py
+++ week1/var-input.py
@@ -5,3 +5,9 @@ name = str(input ())
 location = str(input())
 
 print ("hello", name, "how is the weather in", location, "?")
+
+
+# f string
+
+
+print (f"hello {name}, how is the weather in {location} ?")
