78b7bc7deea4cbac87725eff207018389ea40ebd
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-07 12:15:54 +0530

    week 1

--- week1/var-literals.py
+++ week1/var-literals.py
@@ -5,3 +5,7 @@
 # literals are what can't be changed
 # like variables can have a different value
 
+r = float(input("enter the value of radius: "))
+area = 3.14 *(r**2)
+
+print (f"the value of area of the circle with radius {r} is {area}")
