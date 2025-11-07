e4328051151c08429c1dfb683c184b32f65c74f9
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-07 21:36:47 +0530

    week 1

--- week1/strings.py
+++ week1/strings.py
@@ -3,3 +3,14 @@
 
 # ----- strings 1
 
+a = 'hello'
+b = 'world'
+
+c = a+b     #concatination
+
+print ("the value of the concatinated string is ", c)
+
+# fstring way of doing 
+
+print (f'the value of c is {c}')
+
