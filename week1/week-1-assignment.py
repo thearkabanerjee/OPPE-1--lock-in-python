3e00c15a2de381df052bfcfe637e8aa4725ba206
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-07 22:22:23 +0530

    week 1

---
+++ week1/week-1-assignment.py
@@ -0,0 +1,13 @@
+# the assignment assigned for the week 1
+
+a = '0123456789'
+
+s= a[3] #type = string, prints out 3
+
+b = a[8] # type = string, prints out 8
+
+n = int (s+b) # the value is 38, type = integer 
+
+print (n) #prints out 38 and the type will be integer 
+
+print (type(n)) # prints type as string
