e82dfb0e6dff274a2fad1164a61182b26855b1bf
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 23:17:38 +0530

    week 3

--- week3/test-problems/p3.py
+++ week3/test-problems/p3.py
@@ -4,14 +4,32 @@
 num = int (input ('enter the number to check: '))
 new_num = str(num)
 
+# if (num >= 0):
+#     palindromed = new_num [::-1]
+
+# else:
+#     very_new_num = new_num[1:]
+#     palindromed = '-' + very_new_num [::-1]
+
+# if (int(palindromed) == num):
+#     print ('Palindrome')
+# else:
+#     print ('not a palindrome')
+
+
+
 if (num >= 0):
-    palindromed = new_num [::-1]
+    new_number = new_num
 
 else:
-    very_new_num = new_num[1:]
-    palindromed = '-' + very_new_num [::-1]
+    new_number = new_num[1:]
 
-if (int(palindromed) == num):
+palindromed = new_number [::-1]
+
+if (new_number == (palindromed)):
     print ('Palindrome')
 else:
     print ('not a palindrome')
+
+
+print (new_number)
