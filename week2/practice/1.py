de6379dbe6e15d6ac12c0dfbc788d516827da765
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 19:42:24 +0530

    week 2

--- week2/practice/1.py
+++ week2/practice/1.py
@@ -11,10 +11,10 @@ output1 = '''A single quote ' and a double quote " '''
 output2 = """A forward slash /  and a backward slash \\"""
 
 # Three single quotes ''' and three double quotes """
-output3 = ...
+output3 = """Three single quotes ''' and three double quotes \"\"\" """
 
 # Double forward slash // and Double backward slash \\
-output4 = ...
+output4 = """Double forward slash // and Double backward slash \\"""
 
 
 print (output1)
