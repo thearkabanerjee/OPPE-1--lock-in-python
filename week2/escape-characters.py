23cad421699549f0ac6e2a5d0ae567d509edaee9
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 17:20:14 +0530

    week 2

--- week2/escape-characters.py
+++ week2/escape-characters.py
@@ -19,3 +19,19 @@ print ('my name is arka. \t i am coding right now')
 
 
 # \t acts as a tab
+
+# more space ?
+
+print ('my name is arka. \t \t i am coding right now')
+
+# different lines
+
+print ('hello arka. \n banerjee is calling you')
+
+
+# these work btw
+print ("it's a beautiful day")
+
+print ('we are from "iit" madras')
+
+
