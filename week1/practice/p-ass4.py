4e6a8e964fa56ea58d3cc10ee8be9a6b840b4062
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 10:22:38 +0530

    week 1

--- week1/practice/p-ass4.py
+++ week1/practice/p-ass4.py
@@ -2,3 +2,32 @@
 
 # string concatination, repetation and substring check
 
+# Solve all the below tasks related to string concatenation, repeatition and substring check in strings.
+
+# problem statement 
+
+
+# template code 
+
+# output1 = ... # str: join word1 and word2 with space in between
+
+# output2 = ... # str: join first four letters of word1 and last four letters of word 2 with a hyphen "-" in between
+
+# output3 = ... # str: join the word3 and n1 with a space in between
+
+# output4 = ... # str: just the hypen "-" repeated 50 times
+
+# output5 = ... # str: just the hypen "-" repeated n2 times
+
+# output6 = ... # str: repeat the number n1, n2 times
+
+# are_all_words_equal = ... # bool: True if all three words are equal
+
+# is_word1_comes_before_other_two = ... # bool: True if word1 comes before word2 and word3 assume all words are different
+
+# has_h = ... # bool: True if word1 has the letter h
+
+# ends_with_a = ... # bool: True if word1 ends with letter a or A
+
+# has_the_word_python = ... # bool: True if the sentence has the word python
+
