a954ec44bd1b6d8d79d4c7049d9fa4f4b6034495
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-09 00:09:56 +0530

    week 3

--- week3/for-compute-factorial.py
+++ week3/for-compute-factorial.py
@@ -1,2 +1,11 @@
+# for loop factorial 
 
+n = int(input ('enter the number: '))
+factorial = 1
+i = 1
 
+for i in range (1, n+1): # keep in mind the for loop range
+    factorial = factorial * i
+    i += 1
+
+print (factorial)
