9f320e2007180f3a550985e041d47ad0978620ae
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 22:53:17 +0530

    week 3

--- week3/test-problems/p2.py
+++ week3/test-problems/p2.py
@@ -3,8 +3,15 @@
 
 a = int (input ('the digit entered: '))
 
+if (a >=0):
+
+    a_new = str (a)
+    reversed_a = a_new[::-1]
+
+else:
+    a_new = str(a)
+    a_more_new = a_new[1:]
+    reversed_a = "-" + a_more_new[::-1]
 
 
-a_new = str (a)
-reversed_a = a_new[::-1]
 print (reversed_a)
