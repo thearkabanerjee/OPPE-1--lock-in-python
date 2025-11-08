84e1f3680799b6c93ce49507b0bc3627e84f5c71
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 18:28:39 +0530

    week 2

---
+++ week2/import-2.py
@@ -0,0 +1,13 @@
+# import calendar
+
+from calendar import calendar as c
+
+# print (calendar.calendar (2026))
+
+# print (calendar.month (2925, 7))
+
+
+print (c.month (2925, 7))
+# works this way 
+
+
