b512e9cad2c38800820121d069abe76be13d8b98
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 19:39:45 +0530

    week 2

--- week2/practice/1.py
+++ week2/practice/1.py
@@ -1,2 +1,26 @@
 # practice sessions 
 
+
+# string escaping
+
+
+# A single quote ' and a double quote "
+output1 = '''A single quote ' and a double quote " '''
+
+# A forward slash / and a backward slash \
+output2 = """A forward slash /  and a backward slash \\"""
+
+# Three single quotes ''' and three double quotes """
+output3 = ...
+
+# Double forward slash // and Double backward slash \\
+output4 = ...
+
+
+print (output1)
+
+print (output2)
+
+print (output3)
+
+print (output4)
