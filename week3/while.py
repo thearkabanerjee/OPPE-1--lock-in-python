0feee2ebbde490826f06e3fc19dd6f2761ba74e4
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 21:07:48 +0530

    week 3

---
+++ week3/while.py
@@ -0,0 +1,9 @@
+# while loop 
+
+
+
+independence_year = int (input('find the year of independence: '))
+while (independence_year != 1947):
+    independence_year = int(input('try again buddy: '))
+
+print ('wow you did it.')
