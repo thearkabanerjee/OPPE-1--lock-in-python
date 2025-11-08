41cee21447d241a099f8e8b84b59b0c049e94c5a
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 17:13:58 +0530

    week 2

--- week2/escape-characters.py
+++ week2/escape-characters.py
@@ -9,3 +9,13 @@ print ("it's a beautiful day")
 print ('it\'s a beautiful day')
 
 # this works somehow
+
+
+print('my name is arka. i am coding right now ')
+# additional space 
+
+
+print ('my name is arka. \t i am coding right now')
+
+
+# \t acts as a tab
