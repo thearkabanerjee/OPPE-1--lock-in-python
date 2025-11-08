23a2a97b968363bd34f9dba170008cdce5ba1920
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-08 09:46:07 +0530

    week 1

---
+++ week1/practice/p-ass3.py
@@ -0,0 +1,39 @@
+# strings and slicing
+
+# somehow very important for my preparation
+
+# Solve all the below tasks related to indexing and slicing.
+
+# Keep in mind 🧠 The concept of indexing and slicing will come again in later weeks with list and tuple which are also sequences like string.
+
+# This exercise gives you practice in different indexing and slicing methods.
+
+# Problem Type: Input variable - Output Variable, Hidden suffix for evaluation
+
+
+# Sample inputs (# note: The values given in the prefix code(grey) will be changed by the autograder according to the testcase while running them.
+
+s = "hello pyhton"
+course_code = "24t2cs1002" # 24 - year, t2 - term 2, cs1002 - course id
+# <eoi>
+
+
+
+
+output1 = s[2] # str: get the third character of s
+
+output2 = s[-4] # str: get the fourth last character of s
+
+output3 = ... # str: get the first 3 characters of s
+
+output4 = ... # str: get every second character of s
+
+output5 = ... # str: get the last 3 characters of s
+
+output6 = ... # str: get the reverse of s
+
+course_term = ... # int: get the term of the year as number from course_code
+course_year = ... # int: get the year as two digit number from course_code
+
+
+print (output1 , output2, output3, output4, output5, output6,course_term, course_year)
