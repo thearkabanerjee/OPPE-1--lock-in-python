32d023a7e5eb86b7df161452f0fcc2917fbcc2c9
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-09 10:28:03 +0530

    week 3

---
+++ week3/factorial-for.py
@@ -0,0 +1,19 @@
+# a = int(input ())
+
+# f = 1
+# for i in range (1, a+1):
+#     f = f* i
+#     i +=1
+
+# print (f)
+
+a = int(input())
+
+# sum = 0
+
+# for i in range (a+1):
+#     sum += i
+#     i += 1
+
+# print (sum)
+
