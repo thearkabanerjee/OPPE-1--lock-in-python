647e12ac5d62f60f3585d4a763689ccf52aa53e4
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-09 13:24:46 +0530

    week 3

--- week3/factorial-for.py
+++ week3/factorial-for.py
@@ -1,19 +1,92 @@
-# a = int(input ())
+# # # a = int(input ())
 
-# f = 1
-# for i in range (1, a+1):
-#     f = f* i
-#     i +=1
+# # # f = 1
+# # # for i in range (1, a+1):
+# # #     f = f* i
+# # #     i +=1
 
-# print (f)
+# # # print (f)
 
-a = int(input())
+# # # a = int(input())
 
-# sum = 0
+# # # sum = 0
 
-# for i in range (a+1):
-#     sum += i
-#     i += 1
+# # # for i in range (a+1):
+# # #     sum += i
+# # #     i += 1
 
-# print (sum)
+# # # print (sum)
 
+# # # for i in range (a):
+# # #     for j in range (a):
+# # #         print ("*")
+# # #         j+-1
+# # # i += 1
+
+# # n = 5
+
+# # # for i in range (n):
+
+# # # print ('*'* n)
+# # for i in range (n): # rows
+# #     for j in range (i , n): # columns
+# #         print ("*", end=' ')   
+# #     print() 
+
+
+
+# # a = 5
+
+# # for i in range (a):
+# #     for j in range(i,a):
+# #         print (" ", end =' ')
+# #     print("*", end =' ')
+
+
+# # find all prime numbers less than the entered number
+
+# # a = int(input())
+
+# n = int(input("Enter a number: "))
+
+# # for num in range(2, n):
+# #     for i in range(2, num):
+# #         if num % i == 0:
+# #             break
+# #     else:
+# #         print(num, end =' ')
+
+# for i in range (1, n+1):
+#     # for j in range (i):
+#         print (i)
+
+#     # print ()
+
+
+
+a = int(input ())
+
+# a_new = a. split(' ')
+# print(a_new)
+# if (a_new == ('O-O' or 'O-O-O')):
+#     print ("King \n Rook")
+
+
+
+
+
+
+# a:str = 'hello world'
+# l:list = []
+
+# for i in range (1,a):
+#     if (a% i == 0):
+#         print (i)
+#         l.append(i)
+
+# print (l)
+
+
+import this as this
+
+print (this)
